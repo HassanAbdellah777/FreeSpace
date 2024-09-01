8d829b5cd16fde2c8c499449743d6d0080127fa7
HassanAbdellah777 <112088769+HassanAbdellah777@users.noreply.github.com>
2024-09-02 02:29:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
 //at first time page load
 init();
 
-//Get Posts Element
-let posts = document.querySelector(".posts");
-// Make a request for a user with a given ID
-axios.get("https://tarmeezacademy.com/api/v1/posts").then(function (response) {
-  // handle success
-  let posts = response.data.data;
-  if (Array.isArray(posts)) {
-    for (const post of posts) {
-      addPost(post);
-    }
+//Start Page
+function init() {
+  // Make a request for a user with a given ID
+  axios
+    .get("https://tarmeezacademy.com/api/v1/posts")
+    .then(function (response) {
+      // handle success
+      let posts = response.data.data;
+      if (Array.isArray(posts)) {
+        for (const post of posts) {
+          addPost(post);
+        }
+      } else {
+        addPost(posts);
+      }
+    });
+  setUserUI();
+}
+
+// Set Page UI based on user
+function setUserUI() {
+  let loginDiv = document.getElementById("login-register");
+  let logoutDiv = document.getElementById("logout-div");
+  let newPostDiv = document.getElementById("new-post-div");
+  if (localStorage.getItem("token")) {
+    loginDiv.style.setProperty("display", "none", "important");
+    logoutDiv.style.setProperty("display", "flex", "important");
+    newPostDiv.style.setProperty("display", "flex", "important");
+    let loggedUser = JSON.parse(localStorage.getItem("user"));
+    let imageProfile =
+      typeof loggedUser.profile_image === "string"
+        ? loggedUser.profile_image
+        : "./imgs/user-avatar.png";
+    document.querySelector("#logged-user h6").textContent = loggedUser.username;
+    document.querySelector("#user-avatar img").src = imageProfile;
   } else {
-    addPost(posts);
+    loginDiv.style.setProperty("display", "flex", "important");
+    logoutDiv.style.setProperty("display", "none", "important");
+    newPostDiv.style.setProperty("display", "none", "important");
   }
-});
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
 //adding Post Function
 function addPost(post) {
+  //Get Posts Element
+  let postsElement = document.querySelector(".posts");
+
   let uName = post.author.name;
   let imageProfile =
     typeof post.author.profile_image === "string"
@@ -28,7 +60,6 @@ function addPost(post) {
   let time = post.created_at;
   let commentsCount = post.comments_count;
   let postTags = post.tags;
-
   let tagDiv = document.createElement("div");
   if (postTags.length !== 0) {
     for (let tag of postTags) {
@@ -76,7 +107,7 @@ function addPost(post) {
                   
                 </div>
               </div>`;
-  posts.appendChild(card);
+  postsElement.appendChild(card);
 }
 
 // login Function
@@ -85,122 +116,120 @@ function loginFunction() {
   const pwdInput = document.getElementById("password").value;
   const loginUrl = "https://tarmeezacademy.com/api/v1/login";
   const loginParams = { username: userInput, password: pwdInput };
-  axios.post(loginUrl, loginParams).then((response) => {
-    let token = response.data.token;
-    console.log(token);
-    window.localStorage.setItem("token", token);
-    console.log(window.localStorage.getItem("token"));
-    console.log(response.data.user);
-    window.localStorage.setItem("user", JSON.stringify(response.data.user));
-    let modalElement = document.getElementById("loginModal");
-    let modalInstance = bootstrap.Modal.getInstance(modalElement);
-    modalInstance.hide();
-    toastMsg("Login Successful", "success");
-    // Login Alert
-    // const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-    // const appendAlert = (message, type) => {
-    //   const wrapper = document.createElement("div");
-    //   wrapper.innerHTML = [
-    //     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    //     `   <div>${message}</div>`,
-    //     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    //     "</div>",
-    //   ].join("");
+  axios
+    .post(loginUrl, loginParams)
+    .then((response) => {
+      let token = response.data.token;
+      console.log(token);
+      window.localStorage.setItem("token", token);
+      console.log(window.localStorage.getItem("token"));
+      console.log(response.data.user);
+      window.localStorage.setItem("user", JSON.stringify(response.data.user));
+      let modalElement = document.getElementById("loginModal");
+      let modalInstance = bootstrap.Modal.getInstance(modalElement);
+      modalInstance.hide();
+      toastMsg("Login Successful", "success");
+      // Login Alert
+      // const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+      // const appendAlert = (message, type) => {
+      //   const wrapper = document.createElement("div");
+      //   wrapper.innerHTML = [
+      //     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      //     `   <div>${message}</div>`,
+      //     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      //     "</div>",
+      //   ].join("");
+      //   alertPlaceholder.append(wrapper);
+      //   $("#liveAlertPlaceholder").hide();
+      //   $("#myWish").click(function showAlert() {
+      //     $("#success-alert")
+      //       .fadeTo(2000, 500)
+      //       .slideUp(500, function () {
+      //         $("#liveAlertPlaceholder").slideUp(500);
+      //       });
+      //   });
+      //   // setTimeout(
+      //   //   () => {
 
-    //   alertPlaceholder.append(wrapper);
+      //   //     setTimeout(() => {wrapper.innerHTML = "";}, 2000);
+      //   //   },
+      //   //   2000
+      //   // );
+      //   // const bsAlert = new bootstrap.Alert("#liveAlertPlaceholder");
+      // };
+      // appendAlert("Nice, you triggered this alert message!", "success");
+      // const alertTrigger = document.getElementById("liveAlertBtn");
+      // if (alertTrigger) {
+      //   alertTrigger.addEventListener("click", () => {
+      //     appendAlert("Nice, you triggered this alert message!", "success");
+      //   });
+      // }
 
-    //   $("#liveAlertPlaceholder").hide();
-    //   $("#myWish").click(function showAlert() {
-    //     $("#success-alert")
-    //       .fadeTo(2000, 500)
-    //       .slideUp(500, function () {
-    //         $("#liveAlertPlaceholder").slideUp(500);
-    //       });
-    //   });
-
-    //   // setTimeout(
-    //   //   () => {
-
-    //   //     setTimeout(() => {wrapper.innerHTML = "";}, 2000);
-    //   //   },
-
-    //   //   2000
-    //   // );
-    //   // const bsAlert = new bootstrap.Alert("#liveAlertPlaceholder");
-    // };
-    // appendAlert("Nice, you triggered this alert message!", "success");
-    // const alertTrigger = document.getElementById("liveAlertBtn");
-    // if (alertTrigger) {
-    //   alertTrigger.addEventListener("click", () => {
-    //     appendAlert("Nice, you triggered this alert message!", "success");
-    //   });
-    // }
-
-    setUserUI();
-  });
+      // setUserUI();
+      init();
+    })
+    .catch(function (error) {
+      console.log(error.response.data.message);
+      let errorMsgElement = document.querySelector(".error-login");
+      console.log(errorMsgElement);
+      errorMsgElement.textContent = error.response.data.message;
+    });
 }
-//
 // Register Function
 function registerFunction() {
   const userInput = document.getElementById("register-user-name").value;
   const nameInput = document.getElementById("register-name").value;
   const pwdInput = document.getElementById("register-password").value;
   const emailInput = document.getElementById("email-user").value;
+  const imageFile = document.getElementById("image-file").files[0];
+  console.log(imageFile);
   const registerUrl = "https://tarmeezacademy.com/api/v1/register";
-  const registerParams = {
-    username: userInput,
-    name: nameInput,
-    password: pwdInput,
-    email: emailInput,
-  };
-  axios.post(registerUrl, registerParams).then((response) => {
-    let token = response.data.token;
-    console.log(token);
-    window.localStorage.setItem("token", token);
-    console.log(window.localStorage.getItem("token"));
-    console.log(response.data.user);
-    window.localStorage.setItem("user", JSON.stringify(response.data.user));
-    let modalElement = document.getElementById("registerModal");
-    let modalInstance = bootstrap.Modal.getInstance(modalElement);
-    modalInstance.hide();
-    toastMsg("Registration Completed", "success");
-    setUserUI();
-  });
-}
-
-// Set Page UI based on user
-function setUserUI() {
-  let loginDiv = document.getElementById("login-register");
-  let logoutDiv = document.getElementById("logout-div");
-  let newPostDiv = document.getElementById("new-post-div");
-  if (localStorage.getItem("token")) {
-    loginDiv.style.setProperty("display", "none", "important");
-    logoutDiv.style.setProperty("display", "flex", "important");
-    newPostDiv.style.setProperty("display", "flex", "important");
-    let loggedUser = JSON.parse(localStorage.getItem("user"));
-    let imageProfile =
-      typeof loggedUser.profile_image === "string"
-        ? loggedUser.profile_image
-        : "./imgs/user-avatar.png";
-    document.querySelector("#logged-user h6").textContent = loggedUser.username;
-    document.querySelector("#user-avatar img").src = imageProfile;
-  } else {
-    loginDiv.style.setProperty("display", "flex", "important");
-    logoutDiv.style.setProperty("display", "none", "important");
-    newPostDiv.style.setProperty("display", "none", "important");
-  }
+  // const registerParams = {
+  //   username: userInput,
+  //   name: nameInput,
+  //   password: pwdInput,
+  //   email: emailInput,
+  // };
+  // axios.post(registerUrl, registerParams).then((response) => {}
+  const form = new FormData();
+  form.append("username", userInput);
+  form.append("password", pwdInput);
+  form.append("name", nameInput);
+  form.append("email", emailInput);
+  form.append("image", imageFile);
+  // axios.post("https://example.com", form);
+  axios
+    .post(registerUrl, form)
+    .then((response) => {
+      let token = response.data.token;
+      console.log(token);
+      window.localStorage.setItem("token", token);
+      console.log(window.localStorage.getItem("token"));
+      console.log(response.data.user);
+      window.localStorage.setItem("user", JSON.stringify(response.data.user));
+      let modalElement = document.getElementById("registerModal");
+      let modalInstance = bootstrap.Modal.getInstance(modalElement);
+      modalInstance.hide();
+      toastMsg("Registration Completed", "success");
+      // setUserUI();
+      init();
+    })
+    .catch(function (error) {
+      console.log(error.response.data.message);
+      let errorMsgElement = document.querySelector(".error-register");
+      console.log(errorMsgElement);
+      errorMsgElement.textContent = error.response.data.message;
+    });
 }
 
 //Logout Function
 function logoutFunction() {
   window.localStorage.clear();
   toastMsg("Logged Out", "danger");
-  setUserUI();
+  // setUserUI();
+  init();
 }
-//Load or refresh image
-function init() {
-  setUserUI();
-}
+
 //Toast msg
 function toastMsg(msg, type) {
   const toastDiv = document.getElementById("toast-div");
@@ -224,4 +253,9 @@ function toastMsg(msg, type) {
   const toastLiveExample = document.getElementById("liveToast");
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
   toastBootstrap.show();
+}
+//clear Error Msg in Modals
+function clearErrorMsg() {
+  document.querySelector(".error-login").textContent = "";
+  document.querySelector(".error-register").textContent = "";
 }
